c4d9ebe7bb8ca23d121e313c6c3f6dd8e33ea743
Muzakky Abdullah <muzakkyabdullah93@gmail.com>
2023-12-09 06:07:18 +0700

    menambah try catch

--- controllers/pembayaranController.js
+++ controllers/pembayaranController.js
@@ -3,18 +3,20 @@ const db = require('../models');
 // create tabel pesanan
 
 const addPembayaran = async (req, res) => {
+    try {
+        let info = {
+            virtualAccount             : req.body.virtualAccount,
+            totalHarga                 : req.body.totalHarga,
+            lunasAtauTidak             : req.body.lunasAtauTidak,
+            tabelpemesanId             : req.body.tabelpemesanId,
+        }
     
-    let info = {
-        virtualAccount             : req.body.virtualAccount,
-        totalHarga                 : req.body.totalHarga,
-        lunasAtauTidak             : req.body.lunasAtauTidak,
-        tabelpemesanId             : req.body.tabelpemesanId,
+        const Pembayaran = await db.tabelpembayaran.create(info)
+        res.status(200).send(Pembayaran)
+        console.log(Pembayaran)
+    } catch (error) {
+        console.log(error)
     }
-
-    const Pembayaran = await db.tabelpembayaran.create(info)
-    res.status(200).send(Pembayaran)
-    console.log(Pembayaran)
-
 }
 
 const allPembayaran = async (req, res) => {
