a12828f5b736a240ed258c674ab912d7788d250d
Muzakky Abdullah <muzakkyabdullah93@gmail.com>
2023-12-11 05:41:54 +0700

    mnambah mobil

--- controllers/mobilController.js
+++ controllers/mobilController.js
@@ -6,6 +6,28 @@ const Mobil = db.tabel_mobil
 
 // main work
 
+// add Mobil
+const addMobil = async (req, res) => {
+    
+    try {
+        let info = {
+            namaMobil    : req.body.namaMobil,
+            harga        : req.body.harga,
+            tipeMobil    : req.body.tipeMobil,
+            bagasi       : req.body.bagasi,
+            jumlahMuatan : req.body.jumlahMuatan,
+        }
+    
+        const Mobil = await db.tabel_mobil.create(info)
+        res.status(200).send(Mobil)
+        console.log(Mobil)
+    } catch (error) {
+        console.log(error)
+    }
+
+
+}
+
 // 1. get all mobil
 
 const getAllMobil = async (req, res) => {
