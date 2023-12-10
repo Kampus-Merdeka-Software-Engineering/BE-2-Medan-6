89d77091da9f9d6c3a5f7fd9153f3c87f386fa25
Muzakky Abdullah <muzakkyabdullah93@gmail.com>
2023-12-11 05:51:13 +0700

    ""

--- routes/mobilRouter.js
+++ routes/mobilRouter.js
@@ -2,7 +2,7 @@ const mobilController = require('../controllers/mobilController')
 
 const router = require('express').Router()
 
-router.get('/addMobil', mobilController.addMobil)
+router.post('/addMobil', mobilController.addMobil)
 router.get('/allMobil', mobilController.getAllMobil)
 router.get('/:id', mobilController.getOneMobil)
 
