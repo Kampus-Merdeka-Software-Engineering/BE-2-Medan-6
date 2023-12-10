20bb4074c002d54bb85ba05f3b9f12fc08db0859
Muzakky Abdullah <muzakkyabdullah93@gmail.com>
2023-12-11 05:13:02 +0700

    mengubah router get menjadi post di motor

--- routes/motorRouter.js
+++ routes/motorRouter.js
@@ -2,7 +2,7 @@ const motorController = require('../controllers/motorController')
 
 const router = require('express').Router()
 
-router.get('/addMotor', motorController.addMotor)
+router.post('/addMotor', motorController.addMotor)
 router.get('/allMotor', motorController.getAllMotor)
 router.get('/:id', motorController.getOneMotor)
 
