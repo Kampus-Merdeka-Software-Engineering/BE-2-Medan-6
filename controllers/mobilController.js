e6975adb2acefeaa02e0d787cdc973b033abf174
Muzakky Abdullah <109078655+Zakky1012@users.noreply.github.com>
2023-12-11 09:27:17 +0700

    Update mobilController.js

--- controllers/mobilController.js
+++ controllers/mobilController.js
@@ -6,22 +6,53 @@ const Mobil = db.tabel_mobil
 
 // main work
 
+// add Mobil
+const addMobil = async (req, res) => {
+    
+    try {
+        let info = {
+            namaMobil    : req.body.namaMobil,
+            harga        : req.body.harga,
+            tipeMobil    : req.body.tipeMobil,
+            bagasi       : req.body.bagasi,
+            jumlahMuatan : req.body.jumlahMuatan,
+        }
+    
+        const Mobil = await db.tabel_mobil.create(info)
+        res.status(200).send(Mobil)
+        console.log(Mobil)
+    } catch (error) {
+        console.log(error)
+    }
+
+
+}
+
 // 1. get all mobil
 
 const getAllMobil = async (req, res) => {
+    try {
         let mobils  = await Mobil.findAll({})
         res.status(200).send(mobils)
+    } catch (error) {
+        
+    }
 }
 
 // 2. get single mobil
 
 const getOneMobil = async (req, res) => {
-    let id = req.params.id
-    let mobil  = await Mobil.findOne({ where : { id : id }})
-    res.status(200).send(mobil)
+    try {
+        let id = req.params.id
+        let mobil  = await Mobil.findOne({ where : { id : id }})
+        res.status(200).send(mobil)
+    } catch (error) {
+        
+    }
 }
 
 module.exports = {
+    addMobil,
     getAllMobil,
     getOneMobil
 };
