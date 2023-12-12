64f4f4ec0cdb34b02444e22fff52863464197e04
Muzakky Abdullah <muzakkyabdullah93@gmail.com>
2023-12-12 11:27:56 +0700

    ""

--- controllers/pemesanController.js
+++ controllers/pemesanController.js
@@ -32,7 +32,7 @@ const getOnePemesan = async (req, res) => {
 
 // get all 
 const getAllPemesan = async (req, res) => {
-        let pemesan  = await db.tabelpemesan.findAll({})
+        let pemesan  = await db.pemesan.findAll({})
         res.status(200).send(pemesan)
 }
 
